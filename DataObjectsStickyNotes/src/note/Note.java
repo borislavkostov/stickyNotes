61c6f2cd3a8460af6a185ea80f41b55ca32a1ac5
borislavkostov <borislavkostov97@gmail.com>
2017-01-09 14:06:58 +0200

    Object Note and user

---
+++ DataObjectsStickyNotes/src/note/Note.java
@@ -0,0 +1,29 @@
+package note;
+
+public class Note {
+
+    private String title;
+    private String description;
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
